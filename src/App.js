9fb8a51d3c748fef31e1d276225c84b80fec6572
lokeshkumaran <118963492+lokeshkumaran18@users.noreply.github.com>
2024-11-07 12:13:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       Loki's React APP deployment
+       Loki's React APP dep
       </header>
     </div>
   );
